f90656ebbdc94913168ecb29d7e789ddc9e485ea
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-04 22:47:09 -0400

    Exercise 2.1 CTCI

---
+++ javascript/ch-2/2.1-removeDup.js
@@ -0,0 +1,97 @@
+import * as helpers from './helpers.js';
+
+/**
+ * Remove Dups! Write code to remove duplicates from an unsorted linked list.
+ */ 
+const linkedList = new helpers.DoublyLinkedList;
+linkedList.push(10).push(10).push(15).push(30).push(30).push(15);
+
+/**
+ * @param {DoublyLinkedList} linkedList 
+ * Time Complexity O(n)
+ * Space Complexity O(n)
+ */
+
+function removeDuplicates(linkedList) {
+    if(linkedList.length <= 1) return
+
+    var table = new Map();
+    var tail = linkedList.tail;
+    helper(linkedList.head);
+
+
+    function helper(node) {
+        if(node === null) return
+
+        if(!table.has(node.val)) {
+            table.set(node.val);
+            helper(node.next);
+
+        } else {
+            if(node === tail) {
+                let prevNode = node.prev;
+                // Removing references of the removed node
+                prevNode.next = null;
+            } else {
+
+                let prevNode = node.prev;
+                let nextNode = node.next;
+
+                // Removing references of the removed node
+                prevNode.next = nextNode;
+                nextNode.prev = prevNode;
+            }
+
+            linkedList.length--;
+            helper(node.next);
+        }
+
+    }
+}
+
+/**
+ * @param {DoublyLinkedList} linkedList
+ * Time Complexity O(n²)
+ * Space Complexity O(1)
+ */
+function removeDupWithoutBufffer(linkedList) {
+    var node = linkedList.head;
+
+    while (node !== null) {
+        let nodeInner = node.next;
+
+        while (nodeInner !== null) {
+
+            if (node.val === nodeInner.val) {
+                // Updating the length
+                linkedList.length--;
+
+                if (nodeInner.next === null) {
+                    let prevNode = nodeInner.prev;
+
+                    // Removing references of the removed node
+                    prevNode.next = null;
+                    
+                    // Updating the tail
+                    linkedList.tail = prevNode;
+                } else {
+
+                    let prevNode = nodeInner.prev;
+                    let nextNode = nodeInner.next;
+
+                    // Removing references of the removed node
+                    prevNode.next = nextNode;
+                    nextNode.prev = prevNode;
+                }
+            }
+
+            nodeInner = nodeInner.next;
+        }
+
+        node = node.next;
+    }
+}
+
+removeDupWithoutBufffer(linkedList);
+// removeDuplicates(linkedList);
+console.log(linkedList);
