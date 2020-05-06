cc461d14c1205dc76a936f2098001a8be2a9f548
alan-serrano <alan.serrano.pch@gmail.com>
2020-05-06 09:01:45 -0400

    Second Approach String Compression

--- javascript/ch-1/1.6-stringCompression.js
+++ javascript/ch-1/1.6-stringCompression.js
@@ -5,6 +5,9 @@ of repeated characters. For example, the string aabcccccaaa would become a2blc5a
 the original string. You can assume the string has only uppercase and lowercase letters (a - z).
 */
 
+/* 
+    Using current and next character approach
+*/
 function compress(str) {
     let compressedString = '';
     let counter = 0;
@@ -22,3 +25,38 @@ function compress(str) {
 
     return compressedString;
 }
+
+/* 
+    Using previous and current character approach
+*/
+function compress1(str) {
+    if( str.length === 0 ) return;
+    let count = 0;
+    let prevCh = str[0];
+    let result = '';
+
+    for(let ch of str) {
+        if( prevCh !== ch ) {
+            result += count;
+            count = 0;
+        }
+
+        if( count === 0 ) {
+            result += ch;
+        }
+        
+        count++;
+        prevCh = ch;
+    }
+
+    result += count;
+
+    return result;
+}
+
+(function test() {
+    const str1 = 'aabcccccaaa';
+    
+    console.log(compress(str1));
+    console.log(compress1(str1));
+})()
